15c64a7d5c5f8eb84fd8edb4b59eb19f74d6349f
Shashank <shashank022@gmail.com>
2020-12-17 14:19:01 -0600

    updated new chnages adding Producer to the Kafka config file.

--- src/main/java/com/kafka/producer/config/KakfaConfiguration.java
+++ src/main/java/com/kafka/producer/config/KakfaConfiguration.java
@@ -1,5 +1,6 @@
 package com.kafka.producer.config;
 
+import com.kafka.producer.model.Person;
 import com.kafka.producer.model.User;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
@@ -10,6 +11,7 @@ import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
 import org.springframework.kafka.support.serializer.JsonSerializer;
 
+import java.time.Period;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -27,11 +29,25 @@ public class KakfaConfiguration {
         return new DefaultKafkaProducerFactory<>(config);
     }
 
+    @Bean
+    public ProducerFactory<String, Person> producerPersonFactory() {
+        Map<String, Object> config = new HashMap<>();
+
+        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+
+        return new DefaultKafkaProducerFactory<>(config);
+    }
 
     @Bean
     public KafkaTemplate<String, User> kafkaTemplate() {
         return new KafkaTemplate<>(producerFactory());
     }
 
+    @Bean
+    public KafkaTemplate<String, Person> kafkaPersonTemplate() {
+        return new KafkaTemplate<>(producerPersonFactory());
+    }
 
 }
---
+++ src/main/java/com/kafka/producer/resource/KafkaListenerResource.java
@@ -0,0 +1,51 @@
+package com.kafka.producer.resource;
+
+import com.kafka.producer.model.Person;
+import com.kafka.producer.model.User;
+import com.kafka.producer.service.FileReaderService;
+import com.kafka.producer.service.PersonService;
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("kafka")
+public class KafkaListenerResource {
+
+    private static final Logger log = Logger.getLogger(FileReaderService.class);
+
+    @Autowired
+    private KafkaTemplate<String, User> kafkaTemplate;
+
+    @Autowired
+    private KafkaTemplate<String, Person> kafkaPersonTemplate;
+
+    @Autowired
+    PersonService personService;
+
+    private static final String TOPIC = "Kafka_Example";
+
+    private static final String TOPIC1 = "QPERSON";
+
+    @GetMapping("/publish/{name}")
+    public String post(@PathVariable("name") final String name) {
+        kafkaTemplate.send(TOPIC, new User(name, "Technology", 12000L));
+        return "Published successfully";
+    }
+
+    @GetMapping("/publish/person/{id}")
+    public String postPerson(@PathVariable("id") final Integer id) {
+        if(id != null){
+            Person person = personService.findbyId(id);
+            kafkaPersonTemplate.send(TOPIC1, new Person(id, person.getFirst_name(),person.getLast_name(),person.getEmail(),person.getGender(), person.getAddress(),
+                    person.getCountry(), person.getZipcode(),person.getTimezone()));
+        } else{
+            log.error("The Person record given as Param doesn't exsists in the DB Records : {}");
+        }
+        return "Published successfully";
+    }
+}
