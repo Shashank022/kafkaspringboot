c7aae03e768f175edbe6e66e068aa59094d81922
Shashank <shashank022@gmail.com>
2021-07-16 18:19:52 -0500

    updated new changes tp the application properties

--- src/main/java/com/kafka/producer/service/PersonService.java
+++ src/main/java/com/kafka/producer/service/PersonService.java
@@ -6,23 +6,29 @@ import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class PersonService {
 
     private static final Logger log = Logger.getLogger(PersonService.class);
 
     @Autowired
-    PersonRepository personRepository;
+    PersonRepository pRepo;
 
     /**
      * @param person
      */
     public void save(Person person) {
-        personRepository.save(person);
+        pRepo.save(person);
         log.info("Person Save Successfully {}" + person);
     }
 
     public Person findbyId(Integer id) {
-        return personRepository.findOne(id);
+        return pRepo.findOne(id);
+    }
+
+    public List<Person> getAllPersonsList() {
+        return (List<Person>) pRepo.findAll();
     }
 }
